11c74a57a611f9f363c074399129de6db8172af7
varsha <varsha.bvu@gmail.com>
2013-07-22 10:12:08 -0400

    Repo cleanup.

--- js/global.js
+++ js/global.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	 $("#terms").click(function(){
 	 	if($(this).prop("checked") && $('#news').length==0){
-			$("#conditions").append('<input type="checkbox" id="news"> I would like to receive the upcoming news about Twist Image');
+			$("#conditions").append('<input type="checkbox" id="news">' + 
+				'I would like to receive the upcoming news about Twist Image');
 	 	}
 	});
 });
 
-function emptyText(){
+function emptyUName(){
 	var textbox = $.trim($('#uname').val())
 	if (textbox == ""){
 		alert("Please enter your name.");
